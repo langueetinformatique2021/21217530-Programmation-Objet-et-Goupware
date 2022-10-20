6d901f8c7d731d55322216c7e2f7ce507aee28e7
soken <soken@LAPTOP-0JN2GJJ4>
2022-10-20 10:21:06 +0200

    sokeng

---
+++ td4/src/conditions/td5DATE.java
@@ -0,0 +1,28 @@
+package conditions;
+
+import cours04.Date;
+
+/**
+ * calcul du nombre de jours avant le 14 juillet 2023
+ * 
+ * @author isha
+ *
+ */
+
+
+public class td5DATE {
+
+	public static void main(String[] args) {
+		Date today = new Date(20, 10, 2022);
+		Date fete14 = new Date(14, 7, 2023);
+
+		int nJours = 0;
+		do {
+			today.Incrementer();
+			nJours++;
+		} while (today.CompareTo(fete14));
+		
+		System.out.println(nJours);
+	}
+
+}
