491bb604e54425268fca4a68cff49ca1fb86e8ba
soken <soken@LAPTOP-0JN2GJJ4>
2022-11-17 11:21:43 +0100

    Gregoriancalandar

---
+++ td4/src/atelier7/Gregoriancalender.java
@@ -0,0 +1,39 @@
+package atelier7;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+
+/** dérivée de Calendar implémentant un nouvel attribut cours 
+ * et trois nouvelles méthodes creation() afficher() et duree() */
+public class Calendar extends GregorianCalendar {
+	
+	private boolean cours;
+
+	public Calendar() {
+		Date date = new Date();
+		this.setTime(date);
+	
+	/** affiche la date et l’heure de la création de l’objet */
+	public void creation() {
+		System.out.println("TIME: " + this.getTime());
+	}
+	
+	/** affiche la date et l’heure courante */
+	public void afficher() {
+		Date date = new Date();
+		GregorianCalendar calendar = new GregorianCalendar();
+		calendar.setTime(date);	
+		System.out.println("TIME: " + calendar.getTime());
+	}
+	
+	/** affiche le nombre de secondes depuis la création de l’objet */
+	public void duree() {
+		Date date = new Date();
+		GregorianCalendar calendar = new GregorianCalendar();
+		calendar.setTime(date);	
+		long nbSeconde = (calendar.getTimeInMillis()- this.getTimeInMillis()) / 1000;
+		System.out.println("nbSeconde " + nbSeconde)
+	}
+
+}
