008efd4d53120f47a43efd317c3fdccda1b83e0e
soken <soken@LAPTOP-0JN2GJJ4.lan>
2022-11-30 21:00:45 +0100

    suite jeux de Dé 421

---
+++ td4/src/atelier8/jeuDé.java
@@ -0,0 +1,21 @@
+package atelier8;
+
+import java.util.ArrayList;
+public class jeuDé {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nom = "jeuDé";
+		jeuDé.ld = new jeuDé(nom, 6+1);
+		
+		do {
+			ArrayList<Integer> de = id.jeuDé();
+			print(de);
+		} while (true);
+	}
+		
+	public static void print(Object o) {System.out.println(o);}
+
+	}
+
+
